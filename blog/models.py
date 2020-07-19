3ed749b377951b70888859acc43b4ce9a0aed56d
Matin NazariBaqa <matinnazaribaqa@gmail.com>
2020-07-19 21:58:30 +0430

    added home page

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
-# Create your models here.
+class Blog(models.Model):
+  title = models.CharField(max_length=250, null=True)
+  pub_date = models.DateTimeField(null=True)
+  body = models.TextField(null=True)
+  image = models.ImageField(upload_to='images/', null=True)
